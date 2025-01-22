7c189fa32868f25bcef01c957988eda644025322
gero <cseh.lalika.gergo@gmail.com>
2025-01-22 21:06:09 +0100

    Kivettem a recahptat

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,8 +4,6 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import * as React from 'react';
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import reCAPTCHA, { ReCAPTCHA } from "react-google-recaptcha"
-import { useRef } from "react";
 
 export default function Login(){
     
@@ -18,18 +16,13 @@ export default function Login(){
       const handleMouseUpPassword = (event) => {
         event.preventDefault();
       };
-      const captchaRef = useRef(null);
-      const handleSubmit = (e) =>{
-        e.preventDefault();
-        const token = captchaRef.current.getValue();
-        captchaRef.current.reset();
-    }
+      
         // Gero:
         // nemtudom a faszom roviditesekt elkezdtem egz regist panelt is ahonnan majd mennek a firebasebe a cuccok
     
     return(
         <div className='text-center p-3  justify-evenly flex flex-row'>
-            <div className=' rounded-lg bg-white flex flex-col p-3  m-0 w-fit'>
+            <div className=' rounded-lg bg-gray-800 flex flex-col p-3  m-0 w-fit'>
                 <h1>Sign in</h1>
                 <div className=" content-center">
                         <TextField
@@ -61,11 +54,10 @@ export default function Login(){
                             />
                         </FormControl>
                     </div>
-                        <Link to="/profile"><Button onSubmit={handleSubmit} variant="contained">Login</Button></Link>
-                        <ReCAPTCHA ref={captchaRef} sitekey={process.env.REACT_APP_SITE_KEY}/>
+                        <Link to="/profile"><Button variant="contained">Login</Button></Link>
                     
             </div>
-            <div className="rounded-lg bg-white flex flex-col p-3  m-0 w-fit">
+            <div className="rounded-lg bg-gray-800 flex flex-col p-3  m-0 w-fit">
                 <h1>Sign up</h1>
                 <div className=" content-center">
                         <TextField
