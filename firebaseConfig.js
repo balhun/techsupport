538824f5ba495701483d4ec1eb9f04ad07929de1
gero <cseh.lalika.gergo@gmail.com>
2025-02-09 15:03:43 +0100

    fireBaseConfig upd

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-    authDomain:import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-    projectId:import.meta.env.VITE_FIREBASE_PROJECTID,
-    storageBucket:import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-    messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-    appId:import.meta.env.VITE_FIREBASE_APPID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
+    apiKey: "AIzaSyAPePjJOzHdB8C1UUjgn8SN2RTM0PAxtYM",
+    authDomain: "techsupport-9ff00.firebaseapp.com",
+    projectId: "techsupport-9ff00",
+    storageBucket: "techsupport-9ff00.firebasestorage.app",
+    messagingSenderId: "561309019473",
+    appId: "1:561309019473:web:02cb8f4b552c82fced3770",
+    measurementId: "G-VBXG7LWLM3"
   };
