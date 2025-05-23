14f9eac9ba337978b923b921134601c5bb14ebd0
YoSlurP <cseh.lalika.gergo@gmail.com>
2025-05-23 22:44:51 +0200

    Teszt email ver

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,224 @@
+// import { Stack, TextField, Button, Link, Alert } from "@mui/material";
+// import React, { useState } from "react";
+// import { Navigate, useNavigate } from "react-router-dom";
+// import {
+//   signInWithEmailAndPassword,
+//   signInWithPopup,
+//   createUserWithEmailAndPassword,
+//   updateProfile,
+//   signOut,
+// } from "firebase/auth";
+// import { GoogleAuthProvider } from "firebase/auth";
+// import Passwordshow from "../Componens/Passwordshow";
+
+// export default function Login({
+//   auth,
+//   user,
+//   logout,
+//   //successRegist,
+//   //setSuccessRegist,
+// }) {
+//   const [loginEmail, setLoginEmail] = useState("");
+//   const [loginPassword, setLoginPassword] = useState("");
+
+//   const [loginError, setLoginError] = useState(false);
+
+//   const [felhasznalonev, setFelhasznalonev] = useState("");
+//   const [newEmail, setNewEmail] = useState("");
+//   const [newPassword, setNewPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+
+//   const [error, setError] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const navigate = useNavigate();
+
+//   async function login() {
+//     try {
+//       await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+//       setLoginError(false);
+//       setEmail("");
+//       setPassword("");
+//     } catch (error) {
+//       if (error.code == "auth/invalid-email") {
+//         setLoginError(true);
+//       } else if (error.code == "auth/invalid-credential") {
+//         setLoginError(true);
+//       }
+//     }
+//   }
+
+//   async function regist() {
+//     if (newPassword !== confirmPassword) {
+//       setError(true);
+//       setErrorMessage("Nem egyezik a két jelszó!");
+//     } else {
+//       try {
+//         //setSuccessRegist(false);
+//         const userCredential = await createUserWithEmailAndPassword(
+//           auth,
+//           newEmail,
+//           newPassword
+//         ).then(async (res) => {
+//           const user = userCredential.user;
+//           await updateProfile(user, {
+//             displayName: felhasznalonev,
+//             photoURL: "./blank-pfp.png",
+//           });
+//           //setSuccessRegist(true);
+//         });
+
+//       } catch (err) {
+//         if (felhasznalonev.length < 6) {
+//           setError(true);
+//           setErrorMessage("A nevednek legalább 6 karakterből kell állnia!");
+//         } else if (err.code == "auth/invalid-email") {
+//           setError(true);
+//           setErrorMessage("Helytelen Email cím!");
+//         } else if (err.code == "auth/weak-password") {
+//           setError(true);
+//           setErrorMessage("Jelszó legalább 6 karakterből álljon!");
+//         } else if (err.code == "auth/email-already-in-use") {
+//           setError(true);
+//           setErrorMessage("Az Email cím már használt!");
+//         }
+//       }
+//     }
+//   }
+
+//   async function GoogleLogIn() {
+//     await signInWithPopup(auth, new GoogleAuthProvider());
+//     navigate("/profile", { replace: true });
+//   }
+
+//   return (
+//     <>
+//       {!user ? (
+//         <div className="justify-evenly md:mt-20 mt-2 flex flex-col md:flex-row gap-2 md:items-start items-center">
+//           <Stack
+//             gap={2}
+//             className="island glowing w-fit h-fit flex justify-center items-center p-5 shadow-black"
+//           >
+//             <h1 className="text-center text-2xl">Jelentkezz be itt!</h1>
+//             <TextField
+//               className="w-80 align-middle"
+//               required
+//               data-testid="email-input"
+//               label="Email"
+//               variant="filled"
+//               value={loginEmail}
+//               onChange={(e) => {
+//                 setLoginEmail(e.target.value);
+//                 setLoginError(false);
+//               }}
+//             />
+//             <Passwordshow
+//               value={loginPassword}
+//               testId="password-input-1"
+//               onChange={(e) => {
+//                 setLoginPassword(e.target.value);
+//                 setLoginError(false);
+//               }}
+//               label="Jelszó"
+//             />
+//             <Link
+//               href="/forgotpassword"
+//               sx={{ textDecoration: "none", color: "#4b5563" }}
+//             >
+//               Elfelejtett jelszó
+//             </Link>
+//             {loginError ? (
+//               <Alert severity="error" variant="filled" sx={{ width: "320px" }}>
+//                 Hibás Email vagy jelszó
+//               </Alert>
+//             ) : (
+//               ""
+//             )}
+//             <Link to="/profile">
+//               <Button onClick={login} variant="contained" className="w-80">
+//                 Bejelentkezés
+//               </Button>
+//             </Link>
+//             <p className="">--Or continue with--</p>
+//             <Link to="/profile" sx={{ width: "320px" }}>
+//               <img
+//                 src="./google.png"
+//                 className="flex m-auto cursor-pointer w-4/5"
+//                 onClick={GoogleLogIn}
+//                 alt=""
+//               />
+//             </Link>
+//           </Stack>
+//           <Stack
+//             gap={2}
+//             className="glowing island w-fit h-fit flex flex-col md:flex-row justify-center items-center p-5 mt-8 md:mt-0"
+//           >
+//             <h1 className="text-center text-2xl">Regisztrálj itt!</h1>
+//             <TextField
+//               className="w-80 align-middle"
+//               required
+//               label="Felhasználónév"
+//               variant="filled"
+//               value={felhasznalonev}
+//               onChange={(e) => {
+//                 setFelhasznalonev(e.target.value);
+//                 setError(false);
+//               }}
+//             />
+//             <TextField
+//               className="w-80 align-middle"
+//               required
+//               label="Email"
+//               variant="filled"
+//               value={newEmail}
+//               onChange={(e) => {
+//                 setNewEmail(e.target.value);
+//                 setError(false);
+//               }}
+//             />
+//             <Passwordshow
+//               value={newPassword}
+//               testId="password-input-2"
+//               onChange={(e) => {
+//                 setNewPassword(e.target.value);
+//                 setError(false);
+//               }}
+//               label="Jelszó"
+//             />
+//             <Passwordshow
+//               value={confirmPassword}
+//               testId="password-input-2"
+//               onChange={(e) => {
+//                 setConfirmPassword(e.target.value);
+//                 setError(false);
+//               }}
+//               label="Jelszó megerősítés"
+//             />
+//             {/*error ? (
+//               <Alert severity="error" variant="filled" sx={{ width: "320px" }}>
+//                 {errorMessage}
+//               </Alert>
+//             ) : successRegist ? (
+//               <Alert
+//                 severity="success"
+//                 variant="filled"
+//                 sx={{ width: "320px" }}
+//               >
+//                 Sikeres regisztráció!
+//               </Alert>
+//             ) : (
+//               ""
+//             )*/}
+//             <Button variant="contained" className="w-80" onClick={regist}>
+//               Regisztrálás
+//             </Button>
+//           </Stack>
+//         </div>
+//       ) : (
+//         <Navigate to="/profile" replace />
+//       )}
+//     </>
+//   );
+// }
 import { Stack, TextField, Button, Link, Alert } from "@mui/material";
 import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
@@ -7,20 +228,14 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
   signOut,
+  sendEmailVerification,
+  GoogleAuthProvider,
 } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
 import Passwordshow from "../Componens/Passwordshow";
 
-export default function Login({
-  auth,
-  user,
-  logout,
-  //successRegist,
-  //setSuccessRegist,
-}) {
+export default function Login({ auth, user, logout }) {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
-
   const [loginError, setLoginError] = useState(false);
 
   const [felhasznalonev, setFelhasznalonev] = useState("");
@@ -31,18 +246,33 @@ export default function Login({
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
-  
 
   async function login() {
     try {
-      await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-      setLoginError(false);
-      setEmail("");
-      setPassword("");
-    } catch (error) {
-      if (error.code == "auth/invalid-email") {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      const user = userCredential.user;
+
+      if (!user.emailVerified) {
         setLoginError(true);
-      } else if (error.code == "auth/invalid-credential") {
+        setLoginPassword("");
+        alert("Kérlek, erősítsd meg az emailedet a bejelentkezéshez!");
+        await signOut(auth);
+        return;
+      }
+
+      setLoginError(false);
+      setLoginEmail("");
+      setLoginPassword("");
+      navigate("/profile", { replace: true });
+    } catch (error) {
+      if (
+        error.code === "auth/invalid-email" ||
+        error.code === "auth/invalid-credential"
+      ) {
         setLoginError(true);
       }
     }
@@ -54,38 +284,48 @@ export default function Login({
       setErrorMessage("Nem egyezik a két jelszó!");
     } else {
       try {
-        //setSuccessRegist(false);
         const userCredential = await createUserWithEmailAndPassword(
           auth,
           newEmail,
           newPassword
-        ).then(async (res) => {
-          const user = userCredential.user;
-          await updateProfile(user, {
-            displayName: felhasznalonev,
-            photoURL: "./blank-pfp.png",
-          });
-          //setSuccessRegist(true);
+        );
+
+        const user = userCredential.user;
+
+        await updateProfile(user, {
+          displayName: felhasznalonev,
+          photoURL: "./blank-pfp.png",
         });
 
+        await sendEmailVerification(user);
+        alert(
+          "Sikeres regisztráció! Kérlek, ellenőrizd az emailedet a hitelesítéshez."
+        );
+
+        setFelhasznalonev("");
+        setNewEmail("");
+        setNewPassword("");
+        setConfirmPassword("");
       } catch (err) {
         if (felhasznalonev.length < 6) {
           setError(true);
           setErrorMessage("A nevednek legalább 6 karakterből kell állnia!");
-        } else if (err.code == "auth/invalid-email") {
+        } else if (err.code === "auth/invalid-email") {
           setError(true);
           setErrorMessage("Helytelen Email cím!");
-        } else if (err.code == "auth/weak-password") {
+        } else if (err.code === "auth/weak-password") {
           setError(true);
           setErrorMessage("Jelszó legalább 6 karakterből álljon!");
-        } else if (err.code == "auth/email-already-in-use") {
+        } else if (err.code === "auth/email-already-in-use") {
           setError(true);
           setErrorMessage("Az Email cím már használt!");
+        } else {
+          setError(true);
+          setErrorMessage("Ismeretlen hiba: " + err.message);
         }
       }
     }
   }
-
 
   async function GoogleLogIn() {
     await signInWithPopup(auth, new GoogleAuthProvider());
@@ -130,7 +370,7 @@ export default function Login({
             </Link>
             {loginError ? (
               <Alert severity="error" variant="filled" sx={{ width: "320px" }}>
-                Hibás Email vagy jelszó
+                Hibás Email vagy jelszó, vagy az email nincs megerősítve
               </Alert>
             ) : (
               ""
@@ -140,7 +380,7 @@ export default function Login({
                 Bejelentkezés
               </Button>
             </Link>
-            <p className="">--Or continue with--</p>
+            <p className="">--Vagy folytasd Google-lel--</p>
             <Link to="/profile" sx={{ width: "320px" }}>
               <img
                 src="./google.png"
@@ -195,21 +435,13 @@ export default function Login({
               }}
               label="Jelszó megerősítés"
             />
-            {/*error ? (
+            {error ? (
               <Alert severity="error" variant="filled" sx={{ width: "320px" }}>
                 {errorMessage}
               </Alert>
-            ) : successRegist ? (
-              <Alert
-                severity="success"
-                variant="filled"
-                sx={{ width: "320px" }}
-              >
-                Sikeres regisztráció!
-              </Alert>
             ) : (
               ""
-            )*/}
+            )}
             <Button variant="contained" className="w-80" onClick={regist}>
               Regisztrálás
             </Button>
